174dcf30dbe3d0f0fc379d05efe8c7e91fb435e6
Annderson Pack <annderson.oreto@acad.pucrs.br>
2019-06-12 08:35:04 -0300

    test1

--- ARPool/ARPool/Controller/ConfigureViewController.swift
+++ ARPool/ARPool/Controller/ConfigureViewController.swift
@@ -1,10 +1,3 @@
-//
-//  ConfigureViewController.swift
-//  ARPool
-//
-//  Created by Marcus Vinicius Vieira Badiale on 31/05/19.
-//  Copyright © 2019 Annderson Packeiser Oreto. All rights reserved.
-//
 
 import UIKit
 
--- ARPool/ARPool/Controller/StartScreenViewController.swift
+++ ARPool/ARPool/Controller/StartScreenViewController.swift
@@ -1,10 +1,3 @@
-//
-//  StartScreenViewController.swift
-//  ARPool
-//
-//  Created by Marcus Vinicius Vieira Badiale on 30/05/19.
-//  Copyright © 2019 Annderson Packeiser Oreto. All rights reserved.
-//
 
 import UIKit
 
